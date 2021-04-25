fa3bc34fa4aaa6742a36c86d4c221083a0146d8f
koshmanovia <k0shmanov@yandex.ru>
2021-04-25 19:22:01 +0300

    task 1

---
+++ Глава 1/CalculatorExample/CalculatorExample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace CalculatorExample
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calc c = new Calc();
+            int ans = c.Add(10, 84);
+            Console.WriteLine($"10 + 84 is {ans}.");
+            //Ожидать нажатия пользователем клавиши <Enter> перед завершением работы
+            Console.ReadLine();
+        }
+    }
+    class Calc
+    {
+        public int Add(int x, int y)
+        { return x  + y; }
+
+    }
+}
