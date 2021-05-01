ae2c38853715b820c8cf777c86e78910a2f774e9
koshmanovia <k0shmanov@yandex.ru>
2021-05-01 11:59:05 +0300

    Члены System.Boolean

---
+++ head 3/BoolDataType/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoolDataType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=> bool data type:");
+            Console.WriteLine("bool.FalseString: {0}", bool.FalseString);
+            Console.WriteLine("bool.TrueString: {0}", bool.TrueString);
+            Console.ReadLine();
+        }
+    }
+}
