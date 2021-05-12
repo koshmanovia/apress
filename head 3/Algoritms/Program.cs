20c317f8de0b4c7c2a626286c5be78c579c3f253
koshmanovia <k0shmanov@yandex.ru>
2021-05-12 06:58:35 +0300

    циклы

--- head 3/Algoritms/Program.cs
+++ head 3/Algoritms/Program.cs
@@ -11,6 +11,7 @@ namespace Algoritms
         static void Main(string[] args)
         {
             BinarySearch(20, 2);
+            SelectionSort(65,3,7,9,0);
             Console.ReadLine();
         }
         static void BinarySearch(int high, int item)
@@ -44,6 +45,46 @@ namespace Algoritms
                 }              
             }
             Console.WriteLine();
+        }
+
+        static void SelectionSort(int num1, int num2, int num3, int num4, int num5) 
+        {
+            Console.WriteLine("=> Selection Sort: \n");
+            
+            List<int> arr = new List<int>();
+            List<int> newArr = new List<int>();
+            int smallest = 0;        
+            arr.Add(num1);
+            arr.Add(num2);
+            arr.Add(num3);
+            arr.Add(num4);
+            arr.Add(num5);
+
+            for (int i = 0; i < arr.Count; i++)
+            {
+                smallest = FindSmallest(arr);
+                newArr.Add(arr[smallest]);
+                arr[smallest] = Int32.MaxValue;
+            }
+            for (int i = 0; i < newArr.Count; i++)
+            {
+                Console.WriteLine(newArr[i]);
+            }
+            Console.WriteLine();
+        }
+        static int FindSmallest(List<int> arr) 
+        {
+            var smallest = Int32.MaxValue;
+            var smallestIndex = 0;
+            for (int i = 0;i < arr.Count;i++)
+            {
+                if (arr[i] < smallest)
+                {
+                    smallest = arr[i];
+                    smallestIndex = i;
+                }
+            }
+            return smallestIndex;
         }
     }
 }
---
+++ head 3/IterationsAndDecisions/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IterationsAndDecisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ForLoopExample();
+            ForeachExample();
+            LinqQueryOverInts();
+            WhileLoopExample();
+            DoWhileLoopExample();
+            Console.ReadLine();
+        }
+        static void ForLoopExample()
+        {
+            Console.WriteLine("=> Loop For: \n");
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Number is: {0}",i);
+            }
+
+        }
+        static void ForeachExample()
+        {
+            Console.WriteLine("\n=> Loop ForEach: \n");
+            string[] carTypes = { "Ford", "BMW", "Yugo", "Honda" };
+            foreach (string c in carTypes)
+            {
+                Console.Write("{0}, ",c);
+            }
+            Console.WriteLine();
+            int[] myInts = { 10, 20, 30, 40, 50, 60, 70, 80, 90 };
+            foreach (int i in myInts)
+            {
+                Console.Write("{0}, ",i);
+            }
+
+        }
+        static void LinqQueryOverInts()
+        {
+            Console.WriteLine("\n\n=> Loop ForEach LINQ: \n");
+            int[] numbers = { 10,20,30,40,50,60,70,1,2,3,4,5,6,9,8,7,55};
+            //Запрос LINQ
+            var subset = from i in numbers where i < 10 select i;
+            Console.WriteLine("Values in subset: ");
+            foreach (var i in subset)
+            {
+                Console.Write("{0}, ",i);
+            }
+        }
+        static void WhileLoopExample()
+        {
+            Console.WriteLine("\n => Loop while: \n");
+            string userIsDone = "";
+            while (userIsDone.ToLower() != "yes")
+            {
+                Console.WriteLine("In while loop!");
+                Console.WriteLine("Are you done? [yes][no]: ");
+                userIsDone = Console.ReadLine();
+            }
+        }
+        static void DoWhileLoopExample()
+        {
+            Console.WriteLine("\n => Loop while: \n");
+            string userIsDone = "";
+
+            do
+            {
+                Console.WriteLine("In do/while loop!");
+                Console.WriteLine("Are you done? [yes][no]: ");
+                userIsDone = Console.ReadLine();
+            }
+            while (userIsDone.ToLower() != "yes");
+           
+        }
+    }
+}
