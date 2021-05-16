90eed59e56c226a9ad4564ea8fe4f257e01e0d06
koshmanovia <k0shmanov@yandex.ru>
2021-05-16 21:03:32 +0300

    работа с Enum

---
+++ head 4/FunWithEnums/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithEnums
+{
+    class Program
+    {
+        enum EmpType : byte
+        {
+            Manager = 10,       //=0, если задать значение 200, то след значения будут 200+1
+            Grunt = 1 ,         //=1, 201
+            Contractor = 100,    //=2, 202
+            VicePresident =255  //=3, 203
+        }
+        static void Main()
+        {
+            Console.WriteLine("**** Fun with Enums *****");
+            // Создать переменную типа ЕтрТуре.
+            EmpType еmр = EmpType.Manager;
+            AskForBonus(еmр);
+            Console.ReadLine();
+        }
+        // Перечисления как параметры,
+        static void AskForBonus(EmpType е)
+        {
+            switch (е)
+            {
+                case EmpType.Manager:
+                    Console.WriteLine("How about stock options instead?");
+                    //He желаете ли взамен фондовые опционы?
+                    break;
+                case EmpType.Grunt:
+                    Console.WriteLine("You have got to be kidding...");
+                    // Вы должно быть шутите. . .
+                    break;
+                case EmpType.Contractor:
+                    Console.WriteLine("You already get enough cash...");
+                    // Вы уже получаете вполне достаточно...
+                    break;
+                case EmpType.VicePresident:
+                    Console.WriteLine("VERY GOOD, Sir!");
+                    // Очень хорошо, сэр!
+                    break;
+            }
+        }
+    }
+}
---
+++ head 4/MethodOverLoading/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverLoading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with method overload: *****");
+            Console.WriteLine(Add(23,22));
+            Console.WriteLine(Add(4.3,4.4));
+            Console.WriteLine(Add(900_000_000_000,800_355_546_655));
+            Console.WriteLine(AddWrapper(1,2));
+            Console.ReadLine();
+        }
+        static int Add(int a, int b){return a+b;}
+        static double Add(double a, double b) { return a + b; }
+        static long Add(long a, long b) { return a + b; }
+        static int AddWrapper(int a, int b) 
+        {
+            return Add();
+            int Add()
+            {
+                return (a + b);
+            }
+        }
+    }
+}
