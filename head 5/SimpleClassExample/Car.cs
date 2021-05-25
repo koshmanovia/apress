dae08c1d6a52f27e437999debb3b1efa62689b29
koshmanovia <k0shmanov@yandex.ru>
2021-05-25 22:55:58 +0300

    Работа с инкапсуляцией

---
+++ head 5/SimpleClassExample/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClassExample
+{
+    class Car
+    {
+        public string petName;
+        public int currSpeed;
+        public void PrintState() => Console.WriteLine("{0} is going {1} KMH.", petName, currSpeed);
+        public void Speedup(int delta) => currSpeed += delta;
+        public Car()
+        {
+            petName = "Chuck Norris";
+            currSpeed = 9999;
+        }
+        public Car(string pn) => petName = pn;
+            // Позволяет вызывающему коду установить полное состояние объекта Саr.
+        public Car(string pn, int cs)
+        {
+            petName = pn;
+            currSpeed = cs;
+        }
+    }
+}
---
+++ head 5/SimpleClassExample/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClassExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Class Types *****\n");
+            // Разместить в памяти и сконфигурировать объект Саг.
+            Car myCar = new Car();
+            myCar.petName = "Henry";
+            myCar.currSpeed = 10;
+            // Увеличить скорость автомобиля в несколько раз и вывести новое состояние,
+            for (int i = 0; i <= 10; i++)
+            {
+                myCar.Speedup(13);
+                myCar.PrintState();
+            }
+            // Вызов стандартного конструктора.
+            Car chuck = new Car();
+            chuck.PrintState();
+
+            // Создать объект Car по имени Mary co скоростью 0 миль в час.
+            Car тагу = new Car("Mary");
+            тагу.PrintState();
+            // Создать объект Car по имени Daisy со скоростью 75 миль в час.
+            Car daisy = new Car("Daisy", 75);
+            daisy.PrintState();
+            
+            Motorcycle mc = new Motorcycle();
+            mc.PopAWheely(); 
+
+            Console.ReadLine();
+        }
+    }
+}
