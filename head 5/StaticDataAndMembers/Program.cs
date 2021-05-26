6826f1c2100bd42db6fa763d19913d49a35821d2
koshmanovia <k0shmanov@yandex.ru>
2021-05-26 23:47:22 +0300

    Работа со static

---
+++ head 5/StaticDataAndMembers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticDataAndMembers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Fun with Static Data *****\n");
+            SavingsAccount s1 = new SavingsAccount(50);
+            SavingsAccount s2 = new SavingsAccount(100);
+            // Вывести текущую процентную ставку.
+            Console.WriteLine("Interest Rate is: {0}", SavingsAccount.GetlnterestRate());
+
+            SavingsAccount s3 = new SavingsAccount(10000.75);
+            Console.WriteLine("Interest Rate is: {0}", SavingsAccount.GetlnterestRate());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ head 5/StaticDataAndMembers/SavingsAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticDataAndMembers
+{
+    class SavingsAccount
+    {
+        //данные уровня экземпляра
+        public double currBalance;
+        //Статический эллемент
+        public static double currlnterestRate = 0.04;
+        public SavingsAccount(double balance)
+        {
+            currBalance = balance;
+        }
+        // Статические члены для установки/получения процентной ставки,
+        public static void SetlnterestRate(double newRate) => currlnterestRate = newRate;
+        public static double GetlnterestRate() { return currlnterestRate; }
+    }
+}
