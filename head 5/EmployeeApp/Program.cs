f2822ecb7989bb99ac3c25912458f45fda64c947
koshmanovia <k0shmanov@yandex.ru>
2021-06-01 14:06:13 +0300

    работа с ООП

---
+++ head 5/EmployeeApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***** Fun with Encapsulation *****\n");
+            Employee emp = new Employee("Marvin", 456, 30000);
+            emp.GiveBonus(1000);
+            emp.Displaystats();
+            // Использовать методы get/set для взаимодействия
+            // с именем сотрудника, представленного объектом,
+            emp.SetName("Marv");
+            Console.WriteLine("Employee is named: {0}", emp.GetName());
+           
+            Employee emp2 = new Employee();
+            emp2.SetName("Xena the warrior princess");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ head 5/SimpleUtilityClass/TimeUtilClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using static System.DateTime;
+
+namespace SimpleUtilityClass
+{
+    static class TimeUtilClass
+    {
+        public static void PrintTime() => WriteLine(Now.ToShortTimeString());         
+        public static void PrintDate() => WriteLine(Today.ToShortDateString());         
+    }
+}
