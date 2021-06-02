15a8376ce95a97d2d48bf20b9cc69f2b1d6c6839
koshmanovia <k0shmanov@yandex.ru>
2021-06-02 19:51:02 +0300

    Взаимодествие с автоматическими свойствами

---
+++ head 5/AutoProps/Garage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoProps
+{
+    class Garage
+    {
+        // Скрытое поддерживающее поле int установлено в О!
+        public int NumberOfCars { get; set; } = 0;
+        // Скрытое поддерживающее поле Саг установлено в null!
+        public Car MyAuto { get; set; } = new Car();
+
+        // Для переопределения стандартных значений, присвоенных скрытым
+        // поддерживающим полям, должны использоваться конструкторы,
+        public Garage(){}
+        public Garage(Car car, int number)
+        {
+            MyAuto = car;
+            NumberOfCars = number;
+        }
+    }
+}
---
+++ head 5/AutoProps/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoProps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Automatic Properties *****\n");
+            Car c = new Car();
+            c.PetName = "Frank";
+            c.Speed = 55;
+            c.Color = "Red";
+            Console.WriteLine("Your car is named {0}? That's odd...", c.PetName);
+            c.DisplayStats();
+            
+            // Поместить автомобиль в гараж.
+            Garage g = new Garage();
+            g.MyAuto = c;
+            // Вывести количество автомобилей в гараже.
+            Console.WriteLine("Number of Cars in garage: {0}", g.NumberOfCars);
+            // Вывести название автомобиля.
+            Console.WriteLine("Your car is named: {0}", g.MyAuto.PetName);
+            Console.ReadLine();
+        }
+    }
+}
