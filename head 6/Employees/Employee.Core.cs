7850cb347dc244874f1a0d17b4fd44ff236febd2
koshmanovia <k0shmanov@yandex.ru>
2021-06-15 21:01:19 +0300

    Работа с полиморфными интерфейсами

--- head 6/Employees/Employee.Core.cs
+++ head 6/Employees/Employee.Core.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Employees
 {
-   partial class Employee
+   abstract partial class Employee
     {
         // Поля данных.
         protected string empName;
--- head 6/Employees/Employee.cs
+++ head 6/Employees/Employee.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Employees
 {
-    partial class Employee
+   abstract partial class Employee
     {
         //свойства
         public class BenefitPackage
