87c16397cc1337f35d1dcc1346b5fa6186288b3f
koshmanovia <k0shmanov@yandex.ru>
2021-06-17 19:35:47 +0300

    Изучение явного неявного приведения

--- head 6/Employees/Program.cs
+++ head 6/Employees/Program.cs
@@ -28,5 +28,23 @@ namespace Employees
 
             Console.ReadLine();
         }
+        static void CastingExamples()
+        {
+            // Manager "является" System.Object, поэтому в переменной
+            // типа object можно сохранять ссылку на Manager.
+            object frank = new Manager("Frank Zappa", 9, 3000, 40000, "111-11-1111", 5);
+            Employee moonUnit = new Manager("MoonUnit Zappa", 2, 3001, 20000, "101-11-1321", 1);
+            // PTSalesPerson "является" Salesperson.
+            SalesPerson jill = new PTSalesPerson("Jill", 834, 3002, 100000, "111-12-1119", 90);
+            GivePromotion(moonUnit);
+            GivePromotion(jill);
+            GivePromotion((Manager)frank);
+        }
+        static void GivePromotion(Employee emp)
+        {
+            // Повысить зарплату...
+            // Предоставить место на парковке компании...
+            Console.WriteLine("{0} was promoted!", emp.Name);
+        }
     }
 }
