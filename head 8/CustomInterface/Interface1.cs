38e380edb4cabcba59b09a5dd4a35c53d9c3ba44
koshmanovia <k0shmanov@yandex.ru>
2021-07-03 23:31:07 +0300

    создание Интерфейса

---
+++ head 8/CustomInterface/Interface1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomInterface
+{
+    interface IPointy
+    {
+        // Свойство, поддерживающее чтение и запись, в интерфейсе может выглядеть так:
+        // retType PropName { get; set; }
+        // тогда как свойство только для записи - так:
+        // retType PropName { set; }
+        byte Points { get; }
+    }
+}
---
+++ head 8/CustomInterface/Shape.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customlnterface
+{
+    abstract class Shape
+    {
+        public Shape(string name = "NoName")
+        { PetName = name; }
+        public string PetName { get; set; }
+        // Единственный виртуальный метод,
+        public abstract void Draw();        
+    }
+}
