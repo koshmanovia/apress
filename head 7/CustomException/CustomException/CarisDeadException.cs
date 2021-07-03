f9ab962090ba48d57a1b14a63aa1585e6441d8d4
Илья Кошманов <k0shmanov@yandex.ru>
2021-07-03 13:41:30 +0300

    создание своего исключения

---
+++ head 7/CustomException/CustomException/CarisDeadException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomException
+{
+    class CarisDeadException : ApplicationException
+    {
+        private string messageDetails = String.Empty;
+        public DateTime ErrorTimeStamp { get; set; }
+        public string CauseOfError { get; set; }
+        public CarisDeadException() { }
+        public CarisDeadException(string message, string cause, DateTime time)
+        {
+            messageDetails = message;
+            CauseOfError = cause;
+            ErrorTimeStamp = time;
+        }
+        public override string Message => $"Car Error Message: {messag eDetails}";
+    }
