303ba10973be92c8043bc582bb96b717276e0c19
Илья Кошманов <k0shmanov@yandex.ru>
2021-07-05 22:00:42 +0300

    Множественное наследование интерфейсов

---
+++ head 8/MIInterfaceHierarchy/MIInterfaceHierarchy/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIInterfaceHierarchy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Развлекаюсь с множественным наследованием интерфейсов *****\n");
+            Square s = new Square();
+            Rectangle r = new Rectangle();
+            if (s is IPrintable ip)
+            { ip.Draw(); }
+            if (s is IDrawable id)
+            { id.Draw(); }
+            ((IPrintable)s).Draw();
+            ((IDrawable)s).Draw();
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ head 8/MIInterfaceHierarchy/MIInterfaceHierarchy/Square.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIInterfaceHierarchy
+{
+    class Square : IShape
+    {
+        public int GetNumberOfSides()
+        { return 4;}
+        void IPrintable.Draw()
+        { Console.WriteLine("Drawing IPrintable..."); }
+        void IDrawable.Draw()
+        { Console.WriteLine("Drawing IDrawable..."); }
+        public void Print()
+        { Console.WriteLine("Printing...");}       
+    }
+}
