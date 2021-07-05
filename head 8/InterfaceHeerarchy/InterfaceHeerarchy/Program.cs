afed34f9ca33df0c6a6693c44b9a8274e3a32cdb
Илья Кошманов <k0shmanov@yandex.ru>
2021-07-05 19:45:03 +0300

    работа с иерархией интерфейсов

---
+++ head 8/InterfaceHeerarchy/InterfaceHeerarchy/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceHeerarchy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Simple Interface Hierarchy *****");
+            //Вызов на уровне объекта
+            BitmapImage myBitmap = new BitmapImage();
+            myBitmap.Draw();
+            myBitmap.DrawInBoundingBox(1,1,1,1);
+            myBitmap.DrawUpsideDown();
+            //Получить IAdvancedDraw Явным образом
+            IAdvancedDraw iAdvDraw = myBitmap as IAdvancedDraw;
+            if (iAdvDraw != null)
+                iAdvDraw.DrawUpsideDown();
+            Console.ReadLine();
+        }
+    }
+}
