f4179d6045706230915fd59350ba6d6b85321679
Илья Кошманов <k0shmanov@yandex.ru>
2021-07-06 21:36:40 +0300

    Работа с  клонированием

---
+++ head 8/CloneablePoint/CloneablePoint/Point.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloneablePoint
+{
+    class Point : ICloneable
+    {
+       public int X { get; set; }
+        public int Y { get; set;}
+        public PointDiscription desc = new PointDiscription();
+        public Point(int xPos, int yPos, string petName) { X = xPos; Y = yPos; desc.PetName = petName; }
+        public Point() { }
+        public override string ToString() => $"X = {X}; Y = {Y}; Name = {desc.PetName};\nID = {desc.PointID}\n";
+
+        public object Clone()
+        {
+            Point newPoint = (Point)this.MemberwiseClone();
+            PointDiscription currentDesc = new PointDiscription();
+            currentDesc.PetName = this.desc.PetName;
+            newPoint.desc = currentDesc;
+            return newPoint;
+        }                                                   //public object Clone() => this.MemberwiseClone();
+                                                           //метод выше создает копии всех свойств объекта
+                                                           //ссылочные переменные-члены, все равно будут копировать только ссылку
+                                                           //поэтому данный метод подходит только для клонирования не содержащих
+                                                           //ссылочные типы.
+    }
+}
---
+++ head 8/CloneablePoint/CloneablePoint/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloneablePoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Object Cloning *****\n");
+            Point p1 = new Point(50, 50, "name1");
+            Point p2 = p1;
+            p2.X = 0;
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+            Console.WriteLine("Clonned p3 and stored new Point in p4");
+            Point p3 = new Point(100, 100, "name2");
+            Point p4 = (Point)p3.Clone();
+            Console.WriteLine("Before modification:");
+            Console.WriteLine("p3: {0}", p3);
+            Console.WriteLine("p4: {0}", p4);
+            p4.desc.PetName = "My new Point";
+            p4.X = 999;
+            Console.WriteLine("\nChanged p4.disc.PetName and p4.X");
+            Console.WriteLine("after modification:");
+            Console.WriteLine("p3: {0}", p3);
+            Console.WriteLine("p4: {0}", p4);
+            Console.ReadLine();
+        }
+    }
+}
