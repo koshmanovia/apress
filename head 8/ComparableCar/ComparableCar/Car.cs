39e51786e29f94e0ba019b7b09755b7d757458a9
Илья Кошманов <k0shmanov@yandex.ru>
2021-07-06 22:25:55 +0300

    Начало работы с сортировкой

---
+++ head 8/ComparableCar/ComparableCar/Car.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparableCar
+{
+    class Car : IComparable
+    {
+        // Константа максимальной скорости
+        public const int MaxSpeed = 220;
+        public int CarID { get; set; }
+
+        // свойства автомобиля
+        public int CurrentSpeed { get; set; } = 0;
+        public string PetName { get; set; } = "";
+
+        // не вышел ли автомобиль из строя
+        private bool carIsDead;
+        // Конструкторы
+        public Car() { }
+        public Car(string name, int speed, int id)
+        {
+            CurrentSpeed = speed;
+            PetName = name;
+            CarID = id;
+        }
+        // Проверить не перегрелся ли автомобиль
+        public void Accelerate(int delta)
+        {
+            if (carIsDead)
+                Console.WriteLine("{0} is out of order...", PetName);
+            else
+            {
+                CurrentSpeed += delta;
+                if (CurrentSpeed >= MaxSpeed)
+                {
+                    carIsDead = true;
+                    CurrentSpeed = 0;
+                    Exception ex = new Exception($"{PetName} Has overheated");
+                    ex.HelpLink = "http://www.cars.ru";
+                    ex.Data.Add("TimeStamp", $"The car exploded at {DateTime.Now}");
+                    ex.Data.Add("Cause", "You have a lead foot.");
+                    throw ex;
+                }
+                else
+                    Console.WriteLine("=> CurrentSpeed = {0} KMH", CurrentSpeed);
+            }
+        }
+        int IComparable.CompareTo(object obj)
+        {
+            Car temp = obj as Car;
+            if (temp != null)
+            {
+
+            }
+        }
+    }
+}
---
+++ head 8/ComparableCar/ComparableCar/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparableCar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun With Object Sorting *****\n");
+            Car[] myAutos = new Car[5];
+            myAutos[0] = new Car("Rusty", 80, 1);
+            myAutos[1] = new Car("Mary", 40, 234);
+            myAutos[2] = new Car("Viper", 40, 34);
+            myAutos[3] = new Car("Mel", 40, 4);
+            myAutos[4] = new Car("Chucky", 40, 5);
+            Console.ReadLine();
+        }
+    }
+}
