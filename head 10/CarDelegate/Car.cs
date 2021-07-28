3e17aeef4bd6eb0a1d07bd2113a77a681fbcf69e
koshmanovia <k0shmanov@yandex.ru>
2021-07-28 20:47:20 +0300

    Углубленная работа с делегатами

---
+++ head 10/CarDelegate/Car.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDelegate
+{
+    class Car
+    {
+        //Данные состояния.
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; } = 100;
+        public string PetName{ get; set; }
+
+        //Исправен ли автомобиль?
+        private bool carIsDead;
+        //КОнструкторы класса
+        public Car() { }
+        public Car(string name, int MaxSpd, int currSp)
+        {
+            CurrentSpeed = currSp;
+            MaxSpeed = MaxSpd;
+            PetName = name;
+        }
+        //определяем тип делегада
+        public delegate void CarEngineHandler(string msgForCaller);
+        //определяем переменную-член этого типа делегата
+        private CarEngineHandler listOfHandlers;
+        //добавляем регистрационную функцию для вызывающего кода
+        public void RegisterWithCarEngine(CarEngineHandler methodToCall) 
+        {
+            listOfHandlers = methodToCall;
+        }
+        //реализовываем метод accelerate() для обращения к списку
+        //вызовов делегата в подходящих обстоятельствах
+        public void Accelerate(int delta)
+        {
+            //сообщение о поломке автомобиля
+            if (carIsDead)
+            {
+                if (listOfHandlers != null)
+                {
+                    listOfHandlers("Sorry< this car is dead...");
+                }
+            }
+            else 
+            {
+                CurrentSpeed += delta;
+                //Автомобиль почти сломан?
+                if (10 == (MaxSpeed - CurrentSpeed) && listOfHandlers != null)
+                {
+                    listOfHandlers("Careful buddy! Gonna blow!");
+                }
+                if (CurrentSpeed >= MaxSpeed)
+                {
+                    carIsDead = true;
+                }
+                else
+                    Console.WriteLine("CurrentSpeed ={0}", CurrentSpeed);
+            }
+        }
+    }
+}
---
+++ head 10/CarDelegate/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Delegates as event enablers *****\n");
+            //Создаем объект car
+            Car cl = new Car("SlugBug", 100, 10);
+            //сообщаем car какой метод надо вызвать
+            cl.RegisterWithCarEngine(new Car.CarEngineHandler(OnCarEngineEvent));
+            Console.WriteLine("***** Speeding пр *****");
+            for (int i = 0; i < 6; i++)
+            {
+                cl.Accelerate(20);
+            }             
+            Console.ReadLine();
+        }
+        public static void OnCarEngineEvent(string msg)
+        {
+            Console.WriteLine("\n***** Message From Car Object *****");
+            Console.WriteLine("=> {0}", msg);
+            Console.WriteLine("***********************************\n");
+        }
+    }
+}
