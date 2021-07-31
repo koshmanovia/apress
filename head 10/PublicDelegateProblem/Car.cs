437034ac6a237b3ba329739486b3401937bd4f69
koshmanovia <k0shmanov@yandex.ru>
2021-07-31 21:25:53 +0300

    Проблемы public доступа делегата

---
+++ head 10/PublicDelegateProblem/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublicDelegateProblem
+{
+    class Car
+    {
+        public delegate void CarEngineHandler(string msgForCaller);
+
+        // Теперь это член public!
+        public CarEngineHandler listOfHandlers;
+
+        // Просто вызывать уведомление Exploded
+        public void Accelerate(int delta)
+        {
+            if (listOfHandlers != null)
+                listOfHandlers("Sorry, this car is dead...");
+        }
+    }
+}
---
+++ head 10/PublicDelegateProblem/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublicDelegateProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Agh!  No Encapsulation! *****\n");
+            // Создаем машину
+            Car myCar = new Car();
+
+            // Есть прямой доступ к делегату
+            myCar.listOfHandlers = new Car.CarEngineHandler(CallWhenExploded);
+            myCar.Accelerate(10);
+
+            // Теперь можем присвоить полностью новый объект...
+            // что в лучшем случае сбивает с толку.
+            myCar.listOfHandlers = new Car.CarEngineHandler(CallHereToo);
+            myCar.Accelerate(10);
+
+            // Вызывающий код может также напрямую вызывать делегат
+            myCar.listOfHandlers.Invoke("hee, hee, hee...");
+            Console.ReadLine();
+        }
+
+        static void CallWhenExploded(string msg)
+        { 
+            Console.WriteLine(msg); 
+        }
+
+        static void CallHereToo(string msg)
+        { 
+            Console.WriteLine(msg); 
+        }
+    }
+}
