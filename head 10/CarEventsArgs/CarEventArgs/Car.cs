aace6cc2e9f42b88f0864ef6ac070328ebb28aff
Илья Кошманов <k0shmanov@yandex.ru>
2021-08-02 17:36:47 +0300

    СОздание специальных аргументов событий

---
+++ head 10/CarEventsArgs/CarEventArgs/Car.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarEventsArgs
+{
+    class Car
+    {
+        //Данные состояния.
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; } = 100;
+        public string PetName{ get; set; }
+
+        //Исправен ли автомобиль?
+        private bool carIsDead;
+        //КОнструкторы класса
+        public Car() { }
+        public Car(string name, int MaxSpd, int currSp)
+        {
+            CurrentSpeed = currSp;
+            MaxSpeed = MaxSpd;
+            PetName = name;
+        }
+        //определяем тип делегада и событий
+        public delegate void CarEngineHandler(object sender, CarEventArgs e);
+        public event CarEngineHandler Exploded;
+        public event CarEngineHandler AboutToBlow;
+        public void Accelerate(int delta)
+        {
+            //Если автомобиль сломан инициализировать событие Exploded
+            if (carIsDead)
+            {
+                //if (Exploded != null){Exploded("Sorry, this car is dead...");}
+                //Запись ниже функционально идентична записи выше.
+                Exploded?.Invoke(this, new CarEventArgs("Sorry, this car is dead..."));
+            }
+            else
+            {
+                CurrentSpeed += delta;
+                //Почти сломан?
+                if (10 == MaxSpeed - CurrentSpeed)
+                {
+                    AboutToBlow?.Invoke("Careful buddy! Gonna blow!");
+                }
+                if (CurrentSpeed >= MaxSpeed)
+                {
+                    carIsDead = true;
+                }
+                else
+                {
+                    Console.WriteLine("CurrentSpeed = {0}", CurrentSpeed);
+                }
+            }            
+        }
+    }
+}
---
+++ head 10/CarEventsArgs/CarEventArgs/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarEventsArgs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static void CarAboutToBlow(object sender, CarEventArgs e)
+        {
+            if(sender is Car c)
+            {
+
+            }
+            Console.WriteLine($"{sender} says: {e.msg}");
+        }
+    }
+}
