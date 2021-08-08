43f33afbd04e9a37aacdf5c0a756c50221628d2f
koshmanovia <k0shmanov@yandex.ru>
2021-08-08 20:24:29 +0300

    Изменение кода для работы с лямбда выражениями Шилдт.

---
+++ ShildtC#/UseStatementLambdas/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+//Объявить тип делегата
+delegate string StrMod(string str);
+class DelegateTest
+{
+    static void Main()
+    {
+        //Сконструировали делегаты
+        //StrMod strOp;
+
+        StrMod strReplaceSP = s =>
+            {
+                Console.WriteLine("Замена пробелов дефисами");
+                return s.Replace(' ', '-');
+            };
+
+        StrMod strRemoveSP = s =>
+        {
+            Console.WriteLine("Удаление пробелов");
+            string temp = "";
+            int i;
+            for (i = 0; i < s.Length; i++)
+            {
+                if (s[i] != '-')
+                {
+                    temp += s[i];
+                }
+            }
+            return temp;
+        };
+
+        StrMod strReverce = s =>
+        {
+            string temp = "";
+            int i, j;
+
+            Console.WriteLine("Обращение строки");
+            for (j = 0, i = s.Length - 1; i >= 0; i--, j++)
+            {
+                temp += s[i];
+            }           
+            return temp;
+        };
+        StrMod strOp = strReplaceSP;        
+        string str = strOp("Это простой тест.");
+        Console.WriteLine("Результирующая строка: " + str);
+        Console.WriteLine();
+        strOp = strRemoveSP;
+        str = strOp("Это простой тест.");
+        Console.WriteLine("Результирующая строка: " + str);
+        Console.WriteLine();
+        strOp = strReverce;
+        str = strOp("Это простой тест.");
+        Console.WriteLine("Результирующая строка: " + str);
+        Console.ReadKey();
+    }
+}
