eebc97f5f0d774fa3f5edffa2cbaf517c578a6e1
koshmanovia <k0shmanov@yandex.ru>
2021-08-08 19:00:55 +0300

    Одиночные лямбда выражения по Шилдту

--- ShildtC#/SimpleLambdaDemo/Program.cs
+++ ShildtC#/SimpleLambdaDemo/Program.cs
@@ -1,15 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SimpleLambdaDemo
+delegate int Incr(int v);
+delegate bool IsEven(int v);
+class SimpleLambdaDemo
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Incr incr = count => count + 2;
+        IsEven isEven = result => (result % 2 == 0);
+        Incr decr = count => count - 2;
+        int x = 0;
+        Console.WriteLine("Использование лямбда выражения incr: ");
+        x = -10;
+        while (x <= 0)
         {
+            Console.Write($"{x}\t");
+            x=incr(x);
         }
+        Console.WriteLine("\n");
+
+        Console.WriteLine("Использование лямбда выражения decr: ");
+        x = 10;
+        while (x >= 0)
+        {
+            Console.Write($"{x}\t");
+            x = decr(x);
+        }
+        Console.WriteLine("\n");
+
+        Console.WriteLine("Использование лямбда выражения isEven: ");
+        for (int i = 1; i <= 11; i++)
+        {
+            if (isEven(i))
+            {
+                Console.WriteLine($"{i} четное");
+            }
+            else
+            {
+                Console.WriteLine($"{i} нечетное");
+            }
+        }
+        Console.ReadLine();
     }
 }
+
