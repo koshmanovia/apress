391514180ba4f5e6c18be99ea168b27950b4c132
koshmanovia <k0shmanov@yandex.ru>
2021-08-08 20:03:13 +0300

    Работа с блочными лямбда выражениями

---
+++ ShildtC#/StatementLambdaDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+delegate ulong IntOp(ulong end);
+class StatementLambdaDemo
+{
+    static void Main(string[] args)
+    {
+        //Блочное лямбда-выражение возвращает факториал
+        //передаваемоего ему значания
+        IntOp fact = n =>
+        {
+            ulong r = 1;
+            for (int i = 1; i <= (int)n; i++)
+            {
+                r = (ulong)i * r;
+            }
+            return r;
+        };
+        Console.WriteLine($"Факториал 3 равен: {fact(3)}");
+        Console.WriteLine($"Факториал 5 равен: {fact(5)}");
+        Console.WriteLine($"Факториал 55 равен: {fact(55)}");
+        Console.ReadLine();
+    }
+}
+
