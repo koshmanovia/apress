0527de5f8ea5a810395ed560b953c43d7cc33a0d
koshmanovia <k0shmanov@yandex.ru>
2021-08-12 22:08:14 +0300

    Лямбда выражения по Шилдту

---
+++ ShildtC#/LambdaEventDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+delegate void MyEventHandler(int n);
+class MyEvent
+{
+    public event MyEventHandler SomeEvent;
+    // Этот метод вызывается для запуска события.
+    public void OnSomeEvent(int n)
+    {
+        if (SomeEvent != null)
+            SomeEvent(n);
+    }
+}
+class LambdaEventDemo
+{
+    static void Main(string[] args)
+    {
+        MyEvent evt = new MyEvent();
+        evt.SomeEvent += (n) => Console.WriteLine("Событие получено. Значение равно " + n);
+        // Запустить событие.
+        evt.OnSomeEvent(1);
+        evt.OnSomeEvent(2);
+        Console.ReadLine();
+    }
+}
+
