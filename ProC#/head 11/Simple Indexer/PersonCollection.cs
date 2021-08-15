e3e5fb1be39b0e9494271ee33cb6c2ecee1c3c87
koshmanovia <k0shmanov@yandex.ru>
2021-08-15 14:35:10 +0300

    Понятие индексаторных методов

---
+++ ProC#/head 11/Simple Indexer/PersonCollection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Indexer
+{
+    class PersonCollection : IEnumerable
+    {
+        private ArrayList arPeople = new ArrayList();
+        //Приведение для вызывающего кода
+        public Person GetPerson(int pos) => (Person)arPeople[pos];
+        //вставка только объектов Person
+        public void AddPerson(Person p) => arPeople.Add(p);
+        public void ClearPeople() => arPeople.Clear();       
+
+        public int Count => arPeople.Count;
+
+        public IEnumerator GetEnumerator()
+        {
+          return arPeople.GetEnumerator();
+        }
+        public Person this[int index]
+        {
+            get => (Person)arPeople[index];
+            set => arPeople.Insert(index, value);
+        }
+    }
+}
---
+++ ProC#/head 11/Simple Indexer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Indexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Indexers *****");            
+            PersonCollection myPeople = new PersonCollection();
+            myPeople[0] = new Person("Homer", "Simpson", 40);
+            myPeople[1] = new Person("Marge", "Simpson", 38);
+            myPeople[2] = new Person("Lisa", "Simpson", 9);
+            myPeople[3] = new Person("Bart", "Simpson", 7);
+            myPeople[4] = new Person("Maggie", "Simpson", 2);
+
+            //Получить и отобразить эллементы, используеющие индексатор
+
+            for (int i = 0; i < myPeople.Count; i++)
+            {
+                Console.WriteLine("Person number: {0}", i);
+                Console.WriteLine("Name: {0} {1}", myPeople[i].FirstName, myPeople[i].LastName);
+                Console.WriteLine("Age: {0}\n", myPeople[i].Age);      
+            }
+            Console.ReadLine();
+        }
+    }
+}
