7e2697e3b08a1024a7868c6ff91b3707ced18c43
koshmanovia <k0shmanov@yandex.ru>
2021-08-15 23:25:02 +0300

    перегрузка +,-,++,--, >,<,<=,>=

---
+++ ProC#/head 11/OverloadedOps/Point.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadedOps
+{
+    class Point : IComparable<Point>
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Point(int xPos, int yPos)
+        {
+            X = xPos;
+            Y = yPos;
+        }
+        public override string ToString() => $"[{this.X},{this.Y}]";
+        public int CompareTo(Point other)
+        {
+            if (this.X > other.X && this.Y > other.Y)
+                return 1;
+            if (this.X < other.X && this.Y < other.Y)
+                return 1;
+            else
+                return 0;
+        }
+
+        public static Point operator + (Point p1, Point p2) => new Point(p1.X + p2.X, p1.Y + p2.Y);
+        public static Point operator + (Point p1, int change) => new Point(p1.X + change, p1.Y + change);
+        public static Point operator + (int change, Point p1) => new Point(p1.X + change, p1.Y + change);
+        public static Point operator ++ (Point p1) => new Point(p1.X + 1, p1.Y + 1);
+
+
+        public static Point operator - (Point p1, Point p2) => new Point(p1.X - p2.X, p1.Y - p2.Y);
+        public static Point operator - (Point p1, int change) => new Point(p1.X - change, p1.Y - change);
+        public static Point operator - (int change, Point p1) => new Point(p1.X - change, p1.Y - change);
+        public static Point operator -- (Point p1) => new Point(p1.X - 1, p1.Y - 1);
+
+        public override bool Equals(object o) => o.ToString() == this.ToString();
+        public override int GetHashCode() => this.ToString().GetHashCode();
+
+        public static bool operator == (Point pi, Point p2) => pi.Equals(p2);
+        public static bool operator != (Point pi, Point p2) => !pi.Equals(p2);
+
+        public static bool operator < (Point pi, Point p2) => pi.CompareTo(p2) < 0;
+        public static bool operator > (Point pi, Point p2) => pi.CompareTo(p2) > 0;
+        public static bool operator <= (Point pi, Point p2) => pi.CompareTo(p2) <= 0;
+        public static bool operator >= (Point pi, Point p2) => pi.CompareTo(p2) >= 0;
+    }
+}
---
+++ ProC#/head 11/OverloadedOps/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadedOps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Overloaded Operators *****\n");
+            Point ptOne = new Point(100, 100);
+            Point ptTwo = new Point(40, 40);
+            Console.WriteLine("ptOne = {0} \nptTwo = {1}", ptOne, ptTwo);
+            Console.WriteLine("ptOne + ptTwo = {0}", ptOne + ptTwo);
+            Console.WriteLine("ptOne - ptTwo = {0}", ptOne - ptTwo);
+            Console.WriteLine("ptOne + 10 = {0}", ptOne + 10);
+            Console.WriteLine("ptOne - 10 = {0}", ptOne - 10);
+            Console.WriteLine("10 + ptTwo = {0}", 10 + ptTwo);
+            Console.WriteLine("10 - ptTwo = {0}", 10 - ptTwo);
+            Console.WriteLine();
+            //Перегрузка операции += и -= происходит автоматически
+            Point ptThree = new Point(90, 5);
+            Console.WriteLine("ptThree = {0}", ptThree);
+            Console.WriteLine("ptThree += ptTwo: {0}", ptThree += ptTwo);
+            Console.WriteLine();
+            Point ptFour = new Point(500, 0);
+            Console.WriteLine("ptFour = {0}", ptFour);
+            Console.WriteLine("ptFour -= ptThree: {0}", ptFour -= ptThree);
+            Console.WriteLine();
+            Point ptFive = new Point(1, 1);
+            Console.WriteLine("ptFive++ = {0}", ++ptFive);
+            Console.WriteLine("ptFive-- = {0}", --ptFive);
+            Point ptSix = new Point(20, 20);
+            ptSix++;
+            Console.WriteLine("ptSix++ = {0}", ptSix);
+            ptSix--;
+            Console.WriteLine("ptSix-- = {0}", ptSix);
+            Console.WriteLine();
+            Console.WriteLine("ptOne == ptTwo : {0}", ptOne == ptTwo);
+            Console.WriteLine("ptOne != ptTwo : {0}", ptOne != ptTwo);
+            Console.WriteLine();
+            Console.WriteLine("ptOne < ptTwo : {0}", ptOne < ptTwo);
+            Console.WriteLine("ptOne > ptTwo : {0}", ptOne > ptTwo);
+
+            Console.ReadLine();
+        }
+    }
+}
