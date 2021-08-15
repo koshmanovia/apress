45c08ebd5c1c55e5c9111bfa7395aae295144ae6
koshmanovia <k0shmanov@yandex.ru>
2021-08-15 18:01:15 +0300

    Строковые индексаторы

---
+++ ProC#/head 11/Stringlndexer/PersonCollection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stringlndexer
+{
+    class PersonCollection : IEnumerable
+    {
+        private Dictionary<string, Person> listPeople = new Dictionary<string, Person>();
+        //Приведение для вызывающего кода
+        public Person GetPerson(string name) => (Person)listPeople[name];
+        //вставка только объектов Person
+        public void AddPerson(string name, Person p) => listPeople.Add(name, p);
+        public void ClearPeople() => listPeople.Clear();
+
+        public int Count => listPeople.Count;
+
+        public IEnumerator GetEnumerator() => listPeople.GetEnumerator();
+
+        public Person this[string name]
+        {
+            get => (Person)listPeople[name];
+            set => listPeople[name] = value;
+        }
+    }
+}
+
---
+++ ProC#/head 11/Stringlndexer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stringlndexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Indexers *****\n");
+            PersonCollection myPeople = new PersonCollection();
+            myPeople["Homer"] = new Person("Homer", "Simpson", 40);
+            myPeople["Marge"] = new Person("Marge", "Simpson", 38);
+
+            Person homer = myPeople["Homer"];
+            Console.WriteLine(homer.ToString());
+            Console.WriteLine();
+            Person marge = myPeople["Marge"];
+            Console.WriteLine(marge.ToString());
+            Console.ReadLine();
+        }
+    }
+}
