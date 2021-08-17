dcafd1a8e1d9d62c0ff9d9146328e7cafd5976ba
koshmanovia <k0shmanov@yandex.ru>
2021-08-17 22:35:17 +0300

    Процедуры неявного преобразования

--- ProC#/head 11/CustomConversions/Program.cs
+++ ProC#/head 11/CustomConversions/Program.cs
@@ -27,6 +27,15 @@ namespace CustomConversions
             }
         }
         public override string ToString() => ($"Width = {Width}; Height = {Height}");
+        public static implicit operator Rectangle(Square s)
+        {
+            Rectangle r = new Rectangle
+            {
+                Height = s.Length,
+                Width = s.Length * 2
+            };
+            return r;
+        }
     }
     public struct Square
     {
@@ -53,6 +62,12 @@ namespace CustomConversions
             Square s = new Square { Length = r.Height };
             return s;
         }
+        public static explicit operator Square(int sideLength)
+        {
+            Square newSq = new Square { Length = sideLength };
+            return newSq;
+        }
+        public static explicit operator int(Square s) => s.Length;
     }
     class Program
     {
@@ -70,6 +85,29 @@ namespace CustomConversions
             // Преобразовать Rectangle в Square для вызова метода.
             Rectangle rect = new Rectangle(10, 5);
             DrawSquare((Square)rect);
+            Console.WriteLine();
+
+            //преобразование Square в int и обратно
+            Square sq2 = (Square)6;
+            Console.WriteLine(sq2.ToString());
+            sq2.Draw();
+
+            int side = (int)sq2;
+            Console.WriteLine("Side length of sq2 = {0}", side);
+            Console.WriteLine();
+
+            //неявное преобразование
+            Square s3 = new Square { Length = 7 };
+            Rectangle rect2 = s3;
+            Console.WriteLine();
+            Console.WriteLine("rect2 = {0}", rect2);
+            rect2.Draw();
+
+            Square s4 = new Square { Length = 3 };
+            Rectangle rect3 = (Rectangle)s4;
+            Console.WriteLine("rect3 = {0}", rect3);
+            rect3.Draw();
+
             Console.ReadLine();
         }
         static void DrawSquare(Square sq)
