88d096cf0c2c82b279acb76272e2f8c8b948cc4d
koshmanovia <k0shmanov@yandex.ru>
2021-08-19 00:11:21 +0300

    Разобор расширяемых методов по Рихтеру и Троелсену

---
+++ ProC#/head 11/ExtensionMethods/MyExtension.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace ExtensionMethods
+{
+    static class MyExtension
+    {
+        // Этот метод позволяет объекту любого типа
+        // отобразить сборку, в которой он определен..
+        public static void DisplayDefiningAssembly(this object obj)
+        {
+            Console.WriteLine("{0} lives here: => {1}"
+                , obj.GetType().Name, Assembly.GetAssembly(obj.GetType()).GetName().Name);
+        }
+        // Этот метод позволяет любому целочисленному значению изменить порядок
+        // следования десятичных цифр на обратный. Например, для 56 возвратится 65.
+        public static int ReverseDigits(this int i)
+        {
+            // Транслировать int в string и затем получить все его символы.
+            char[] digits = i.ToString().ToCharArray();
+            Array.Reverse(digits);
+
+            //поместить обратно в строку
+            string newDigits = new string(digits);
+
+            //Возвратить модифицированную строку как int                      
+            return int.Parse(newDigits);
+        }
+    }
+}
---
+++ ProC#/head 11/ExtensionMethods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder("sss.");
+            //Int32 index = StringBuilderExtensions.IndexOf(sb.Replace(".", "!"), '!');
+            Int32 index = sb.Replace(".", "!").
+            Console.WriteLine(index);
+            Console.ReadLine();
+        }
+    }
+    public static class StringBuilderExtensions
+    {
+        public static Int32 IndexOf(this StringBuilder sb, Char value)
+        {
+            for (Int32 index = 0; index < sb.Length; index++)
+                if (sb[index] == value) return index;
+            return -1;
+        }
+    }
+}
