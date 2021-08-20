c2824917530046fd5740695ba15a92e0fdeda77b
Илья Кошманов <k0shmanov@yandex.ru>
2021-08-20 22:56:09 +0300

    Расширенипе интерфейсов

---
+++ ProC#/head 11/InterfaceExtensions/InterfaceExtensions/AnnoyingExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExtensions
+{
+    static class AnnoyingExtensions
+    {
+        public static void PrintDataAndBeep(this System.Collections.IEnumerable iterator)
+        {
+            foreach (var item in iterator)
+            {
+                Console.Write($"{item} ");
+                Console.Beep();
+            }
+
+        }
+    }
+}
---
+++ ProC#/head 11/InterfaceExtensions/InterfaceExtensions/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExtensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Extending Interface Compatible Types *****\n");
+            //System.Array реализует IEnumerable
+            string[] data = { "Wow", "this", "is", "sort", "of", "annoying", "but", "in", "a", "weird", "way", "fun!" };
+            data.PrintDataAndBeep();
+            Console.WriteLine();
+            //List<T> реализует Ienumerable!
+            List<int> myInts = new List<int>() { 1, 2, 3, 4, 5 };
+            myInts.PrintDataAndBeep();
+            Console.ReadLine();
+        }
+    }
+}
