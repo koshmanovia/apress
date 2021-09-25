31109aa4c4744c1aa60f45b154633931d9d0ee3e
Koshmanov Ilya <k0shmanov@yandex.ru>
2021-09-25 22:08:29 +0300

    Linq по Шилдту

---
+++ ProC#/head 12/FunWithLinqExpressions/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithLinqExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*******Fun with Query Expressions * ****\n");
+            // Этот массив будет основой для тестирования. . .
+            ProductInfo[] itemsInStock = new[] 
+            {
+                new ProductInfo{ Name = "Mac's Coffee", Description = "Coffee with TEETH", NumberlnStock = 24},
+                new ProductInfo{Name = "Milk Maid Milk", Description = "Milk cow's love", NumberlnStock = 100},
+                new ProductInfo{ Name = "Pure Silk Tofu", Description = "Bland as Possible", NumberlnStock = 120}, 
+                new ProductInfo{ Name = "Crunchy Pops", Description = "Cheezy, peppery goodness", NumberlnStock = 2},
+                new ProductInfo{ Name = "RipOff Water", Description = "From the tap to your wallet", NumberlnStock = 100},
+                new ProductInfo{ Name = "Classic Valpo Pizza", Description = "Everyone loves pizza'", NumberlnStock = 73}
+            };
+            SelectEverything(itemsInStock);
+            Console.WriteLine();
+            ListProductNames(itemsInStock);
+            Console.ReadLine();
+        }
+        static void SelectEverything(ProductInfo[] products)
+        {
+            Console.WriteLine("All product details");
+            var allProducts = from p in products select p;
+            foreach (var prod in allProducts)
+            {
+                Console.WriteLine(prod.ToString());
+            }
+        }
+        static void ListProductNames(ProductInfo[] products)
+        {
+            // Теперь получить только наименования товаров.
+            Console.WriteLine("Only product names:");
+            var names = from p in products select p.Name;
+            foreach (var n in names)
+            {
+                Console.WriteLine("Name: {0}", n);
+            }
+        }
+    }
+
+    class ProductInfo
+    {
+        public string Name { get; set; } = "";
+        public string Description { get; set; } = ""; 
+        public int NumberlnStock { get; set; } = 0;
+        public override string ToString() => $"Name = {Name}, Descnption = {Description}, Number in Stock = {NumberlnStock}";
+    }
+}
---
+++ ProC#/head 12/LinqRetValues/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqRetValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** LINQ Return Values *****\n");
+            IEnumerable<string> subset = GetStringSubset();
+            foreach (string item in subset)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+        static IEnumerable<string> GetStringSubset()
+        {
+            string[] colors = {"Light Red", "Green", "Yellow", "Dark Red", "Red", "Purple"};
+            // Обратите внимание, что subset является
+            // совместимым с IEnumerable<string> объектом.
+            IEnumerable<string> theRedColors = from c in colors where c.Contains("Red") select c;
+            return theRedColors;
+        }
+        static string[] GetStringSubsetAsArray()
+        {
+            string[] colors = {"Light Red", "Green",
+                "Yellow", "Dark Red", "Red", "Purple"};
+            var theRedColors = from c in colors
+                where c.Contains("Red")
+                select c;
+            // Отобразить результаты в массив,
+            return theRedColors.ToArray();
+        }
+    }
+}
+
+
