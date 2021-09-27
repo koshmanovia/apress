077c112c99aed017750389c855cc159f18ed6301
Koshmanov Ilya <k0shmanov@yandex.ru>
2021-09-27 20:20:44 +0300

    Изучение LINQ по Троелсену и Джепиксу

--- ProC#/head 12/FunWithLinqExpressions/Program.cs
+++ ProC#/head 12/FunWithLinqExpressions/Program.cs
@@ -7,23 +7,24 @@ using System.Threading.Tasks;
 namespace FunWithLinqExpressions
 {
     class Program
-    {
-        static void Main(string[] args)
+    {        static void Main(string[] args)
         {
             Console.WriteLine("*******Fun with Query Expressions * ****\n");
             // Этот массив будет основой для тестирования. . .
             ProductInfo[] itemsInStock = new[] 
             {
-                new ProductInfo{ Name = "Mac's Coffee", Description = "Coffee with TEETH", NumberlnStock = 24},
-                new ProductInfo{Name = "Milk Maid Milk", Description = "Milk cow's love", NumberlnStock = 100},
-                new ProductInfo{ Name = "Pure Silk Tofu", Description = "Bland as Possible", NumberlnStock = 120}, 
-                new ProductInfo{ Name = "Crunchy Pops", Description = "Cheezy, peppery goodness", NumberlnStock = 2},
-                new ProductInfo{ Name = "RipOff Water", Description = "From the tap to your wallet", NumberlnStock = 100},
-                new ProductInfo{ Name = "Classic Valpo Pizza", Description = "Everyone loves pizza'", NumberlnStock = 73}
+                new ProductInfo{ Name = "Mac's Coffee", Description = "Coffee with TEETH", NumberInStock = 24},
+                new ProductInfo{Name = "Milk Maid Milk", Description = "Milk cow's love", NumberInStock = 100},
+                new ProductInfo{ Name = "Pure Silk Tofu", Description = "Bland as Possible", NumberInStock = 120}, 
+                new ProductInfo{ Name = "Crunchy Pops", Description = "Cheezy, peppery goodness", NumberInStock = 2},
+                new ProductInfo{ Name = "RipOff Water", Description = "From the tap to your wallet", NumberInStock = 100},
+                new ProductInfo{ Name = "Classic Valpo Pizza", Description = "Everyone loves pizza'", NumberInStock = 73}
             };
             SelectEverything(itemsInStock);
             Console.WriteLine();
             ListProductNames(itemsInStock);
+            Console.WriteLine();
+            GetOverstock(itemsInStock);
             Console.ReadLine();
         }
         static void SelectEverything(ProductInfo[] products)
@@ -45,13 +46,27 @@ namespace FunWithLinqExpressions
                 Console.WriteLine("Name: {0}", n);
             }
         }
+        static void GetOverstock(ProductInfo[] products)
+        {
+            Console.WriteLine("The overstock items!");
+            var overstock = from p in products where p.NumberInStock > 25 select p;
+            foreach (ProductInfo c in overstock)
+            {
+                Console.WriteLine(c.ToString());
+            }
+        }
+        static void GetNameAndDescriptions(ProductInfo[] products)
+        {
+            Console.WriteLine("Names and Descriptions");
+
+        }
     }
 
     class ProductInfo
     {
         public string Name { get; set; } = "";
         public string Description { get; set; } = ""; 
-        public int NumberlnStock { get; set; } = 0;
-        public override string ToString() => $"Name = {Name}, Descnption = {Description}, Number in Stock = {NumberlnStock}";
+        public int NumberInStock { get; set; } = 0;
+        public override string ToString() => $"Name = {Name}, Descnption = {Description}, Number in Stock = {NumberInStock}";
     }
 }
