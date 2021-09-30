93fd5dec1296599bb74a5392768aca7f6b252182
Koshmanov Ilya <k0shmanov@yandex.ru>
2021-09-30 21:03:58 +0300

    Завершение  главы LINQ по Троелсену и Джепиксу
    Изучение как работаю методы Enumerable и делегатов

--- ProC#/head 12/LinqUsingEnumerable/LinqUsingEnumerable/Program.cs
+++ ProC#/head 12/LinqUsingEnumerable/LinqUsingEnumerable/Program.cs
@@ -14,6 +14,13 @@ namespace LinqUsingEnumerable
             QueryStringWithOperators(currentVideoGames);
             System.Console.WriteLine();
             QuerySttringWithEnumerableAndLambdas(currentVideoGames);
+            Console.WriteLine();
+            QueryStringsWithEnumerableAndLambdas2(currentVideoGames);
+            Console.WriteLine();
+            QueryStringsWithAnonymousMethods(currentVideoGames);
+            Console.WriteLine();
+            VeryComplexQueryExpression.QueryStringsWithRawDelegates(currentVideoGames);
+            Console.WriteLine();
             Console.ReadLine();
         }
 
@@ -38,5 +45,54 @@ namespace LinqUsingEnumerable
                 Console.WriteLine("Item: {0}", game);
             }
         }
+        static void QueryStringsWithEnumerableAndLambdas2(string[] currentVideoGames)
+        {
+            Console.WriteLine("***** Using Enumerable / Lambda Expressions 2*****\n");
+            // Разбить на части.
+            var gamesWithSpaces = currentVideoGames.Where(game => game.Contains(" "));
+            var orderedGames = gamesWithSpaces.OrderBy(game => game);
+            var subset = orderedGames.Select(game => game);
+            foreach (var game in subset)
+                Console.WriteLine("Item: {0}", game);
+            Console.WriteLine();
+        }
+        static void QueryStringsWithAnonymousMethods(string[] currentVideoGames)
+        {
+            Console.WriteLine("***** Using Anonymous Methods *****");
+            Func<string, bool> searchFilter = delegate (string game) { return game.Contains(" "); };
+            Func<string, string> itemToProcess = delegate (string s) { return s; };
+            // Передать делегаты в методы класса Enumerable.
+            var subset = currentVideoGames.Where(searchFilter).OrderBy(itemToProcess).Select(itemToProcess);
+            // Вывести результаты,
+            foreach (var game in subset)
+            {
+                Console.WriteLine("Item: {0}", game);
+            }
+            Console.WriteLine();
+        }
+        class VeryComplexQueryExpression
+        {
+            public static void QueryStringsWithRawDelegates(string[] currentVideoGames)
+            {
+                Console.WriteLine("***** Using Raw Delegates *****");
+                // Построить необходимые делегаты Funco.
+                Func<string, bool> searchFilter = new Func<string, bool>(Filter);
+                Func<string, string> itemToProcess = new Func<string, string>(Processltem);
+                // Передать делегаты в методы класса Enumerable.
+                var subset = currentVideoGames.Where(searchFilter).OrderBy(itemToProcess).Select(itemToProcess);
+                // Вывести результаты
+                foreach (var game in subset)
+                {
+                    Console.WriteLine("Item: {0}", game);
+                }
+                Console.WriteLine();
+            }
+            // Цели делегатов.
+            public static bool Filter(string game) { return game.Contains(" "); }
+            public static string Processltem(string game) { return game; }
+        }
+
     }
+
 }
+
