2ff2f07493595229f12b3bd2780a925e1848de91
Koshmanov Ilya <k0shmanov@yandex.ru>
2021-10-06 23:45:21 +0300

    Подготовка к изучению  GC

---
+++ ProC#/head 13/SimpleGC/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleGC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** GC Basics *****") ;
+            Car refToMyCar = new Car("Zippy", 50);
+            Console.WriteLine(refToMyCar.ToString());
+            Console.ReadLine();
+        }
+
+    }
+}
