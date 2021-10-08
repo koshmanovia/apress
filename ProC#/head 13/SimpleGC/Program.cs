86435823d8da340e0e011ea4d8eef18542526faf
Koshmanov Ilya <k0shmanov@yandex.ru>
2021-10-08 13:53:20 +0300

    GC по Троелсену и Джепиксу

--- ProC#/head 13/SimpleGC/Program.cs
+++ ProC#/head 13/SimpleGC/Program.cs
@@ -19,6 +19,31 @@ namespace SimpleGC
             Console.WriteLine(refToMyCar.ToString());
             // Вывести поколение объекта refToMyCar.
             Console.WriteLine("Generation of refToMyCar is: {0}", GC.GetGeneration(refToMyCar));
+            //Создать большое количество объектов для тестирования
+            object[] tonsOfObjects = new object[50000];
+            for (int i = 0; i < 50000; i++)
+            {
+                tonsOfObjects[i] = new object();
+            }
+            //выполнить уборку мусора только для объектов покаления 0.
+            GC.Collect(0, GCCollectionMode.Forced);
+            GC.WaitForPendingFinalizers();
+            //Вывод покаление refToMyCar.
+            Console.WriteLine($"Generation of refToMyCar is {GC.GetGeneration(refToMyCar)}");
+            //Посмотреть существует ли еще tonsOfObjects[9000].
+            if (tonsOfObjects[9000] != null)
+            {
+                Console.WriteLine($"Generations of tonsOfObjects[9000] is: {GC.GetGeneration(tonsOfObjects[9000])}");
+            }
+            else
+            {
+                Console.WriteLine("tonsOfObjects[9000] is no longer alive");
+            }
+            //Вывести количество проведенных сборок мусора для разных поколений.
+            Console.WriteLine("\nGen 0 has been swept {0} times", GC.CollectionCount(0));
+            Console.WriteLine("\nGen 1 has been swept {0} times", GC.CollectionCount(1));
+            Console.WriteLine("\nGen 2 has been swept {0} times", GC.CollectionCount(2));
+
             Console.ReadLine();
         }
     }
