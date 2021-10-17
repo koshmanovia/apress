dee3ac9d2055516fe4d21a856a6305caf8006e37
Koshmanov Ilya <k0shmanov@yandex.ru>
2021-10-17 16:49:16 +0300

    Изучение сборок по Троелсену и Джепиксу

--- ProC#/head 13/LazyObjectlnstantiation/Program.cs
+++ ProC#/head 13/LazyObjectlnstantiation/Program.cs
@@ -10,6 +10,55 @@ namespace LazyObjectlnstantiation
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun with Lazy Instantiation *****\n");
+            // Память под объект AllTracks здесь не выделяется!
+            MediaPlayer myPlayer = new MediaPlayer();
+            myPlayer.Play();
+            // Размещение объекта AllTracks происходит
+            // только в случае вызова метода GetAllTracks().
+            MediaPlayer yourPlayer = new MediaPlayer();
+            AllTracks yourMusic = yourPlayer.GetAllTracks();
+            Console.ReadLine();
+        }
+    }
+    // Представляет одиночную композицию.
+    class Song
+    {
+        public string Artist { get; set; }
+        public string TrackName { get; set; }
+        public double TrackLength { get; set; }
+    }
+    // Представляет все композиции в проигрывателе.
+    class AllTracks
+    {
+        // Наш проигрыватель может содержать
+        // максимум 10 000 композиций,
+        private Song[] allSongs = new Song[10000];
+        public AllTracks()
+        {
+            // Предположим, что здесь производится
+            // заполнение массива объектов Song.
+            Console.WriteLine("Filling up the songs!");
+        }
+    }
+    // Объект MediaPlayer имеет объекты AllTracks.
+    class MediaPlayer
+    {
+        // Предположим, что эти методы делают что-то полезное,
+        public void Play() { /* Воспроизведение композиции */ }
+        public void Pause() { /* Пауза в воспроизведении */ }
+        public void Stop() { /* Останов воспроизведения */ }
+        private Lazy<AllTracks> allSongs = new Lazy<AllTracks>( () =>
+        {
+            Console.WriteLine("Creating AllTracks object!");
+            return new AllTracks();
+        }
+        );
+
+        public AllTracks GetAllTracks()
+        {
+            // Возвратить все композиции,
+            return allSongs.Value;
         }
     }
 }
