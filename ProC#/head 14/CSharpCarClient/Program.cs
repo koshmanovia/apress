49cb4036598053904e40a57fa93a4598625662de
Koshmanov Ilya <k0shmanov@yandex.ru>
2021-11-06 23:33:22 +0300

    Работа с клиентскими сборками c#

---
+++ ProC#/head 14/CSharpCarClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarLibrary;
+
+namespace CSharpCarClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** C# CarLibrary Client App *****");
+            // Создать объект SportsCar.
+            SportsCar viper = new SportsCar("Viper", 240, 40);
+            viper.TurboBoost();
+            // Создать объект MiniVan.
+            MiniVan mv = new MiniVan();
+            mv.TurboBoost();
+            Console.WriteLine("Done. Press any key to terminate");
+            Console.ReadLine();
+
+        }
+    }
+}
