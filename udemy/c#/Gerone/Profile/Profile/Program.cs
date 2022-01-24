437bc07cf05a989ffc8d08ea8b6587ba6ead1646
Koshmanov Ilya <k0shmanov@yandex.ru>
2022-01-24 22:40:39 +0300

    udemy home work

---
+++ udemy/c#/Gerone/Profile/Profile/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input your surname");
+            string surname = Console.ReadLine();   
+            Console.WriteLine("Input your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Input your age");
+            string age = Console.ReadLine();
+            Console.WriteLine("Input your weight kg");
+            string weight = Console.ReadLine(); 
+            Console.WriteLine("Input your height meter's");
+            string height = Console.ReadLine(); 
+
+            double IMT = double.Parse(weight) / (double.Parse(height) * double.Parse(height));
+            
+            Console.WriteLine($"Your Profile:\n" +
+                $"Fill name: {surname}, {name}\n" +
+                $"Age: {age}\n" +
+                $"Weight: {weight}\n" +
+                $"Height: {height}\n" +
+                $"Body Mass Index: {IMT:f2}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ udemy/c#/Gerone/Strengle/Strengle/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strengle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("input a,b,c");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+            if (a + b > c && a + c > b && b + c > a)
+            {
+                double p = (a + b + c) / 2;
+                double s = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+                string result = string.Format($"{s:f2}");
+                Console.WriteLine("S = {0}", result);
+            }
+            else 
+            {
+                Console.WriteLine("Thriangle not alive");
+            }
+            Console.ReadLine();
+        }
+    }
+}
