a947064c0826ed6a2bc8cf133a49a37f3b165032
Koshmanov Ilya <k0shmanov@yandex.ru>
2022-01-31 14:42:54 +0300

    home work midlenum

---
+++ udemy/c#/CalcMidle/CalcMidle/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcMidle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            int temp = default;
+            for (int i = 0; i < 10; i++)
+            {
+               Console.WriteLine("Введите целое цисло: ");
+               temp = (int.Parse(Console.ReadLine()));
+               if (temp == 0)
+               {
+                   break;
+               }
+               else
+               {
+                   list.Add(temp);
+               }                
+            }
+            temp = 0;
+            int iteratorThree = default;
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] % 3 == 0)
+                { 
+                    temp += list[i];
+                    iteratorThree++;
+                }
+            }
+            double average = (double)temp / iteratorThree;
+            Console.WriteLine(average);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ udemy/c#/CalcMidle/CalcMidle/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("CalcMidle")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalcMidle")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("f7a12664-a94a-4c29-aee3-518b25567ddf")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
