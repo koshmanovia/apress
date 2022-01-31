8bfa637c78025617fe33bda9c9dbb6424489007f
Koshmanov Ilya <k0shmanov@yandex.ru>
2022-01-31 12:52:39 +0300

    Home Work Fibonacci num

---
+++ udemy/c#/FibonacciNum/FibonacciNum/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciNum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество чисел Фибаначчи");
+            int count  = int.Parse(Console.ReadLine());
+            List<int> arrayFibanacci = new List<int>();
+            for (int i = 0; i < count; i++)
+            {
+                if (i == 0)
+                {
+                    arrayFibanacci.Add(0);
+                }
+                else 
+                    if (i == 1)
+                    {
+                    arrayFibanacci.Add(1);
+                    }
+                    else
+                    {
+                    arrayFibanacci.Add(arrayFibanacci[i - 1] + arrayFibanacci[i - 2]);
+                    }
+            }
+            Console.Clear();
+            foreach (int fibonacciNum in arrayFibanacci)
+            {
+                Console.Write(fibonacciNum + " ");
+            }                        
+            Console.ReadLine();
+        }
+    }
+}
