2b227e99dba32c982ab9b6a65b2afd8e4106559a
Koshmanov Ilya <k0shmanov@yandex.ru>
2022-04-27 22:21:13 +0300

    рабочий вариант игры виселица

--- udemy/c#/Hangman_Game_HW/Hangman_Game_HW/Program.cs
+++ udemy/c#/Hangman_Game_HW/Hangman_Game_HW/Program.cs
@@ -12,7 +12,7 @@ namespace Hangman_Game_HW
         static void Main(string[] args)
         {
             var h = new Hangman();
-            Console.WriteLine(h.numStrWord + "\n" + h.secretWord);
+            //Console.WriteLine(h.numStrWord + "\n" + h.secretWord);
             h.StartGame();
             Console.ReadKey();
         }
@@ -29,7 +29,7 @@ namespace Hangman_Game_HW
         public Hangman()
         {
             numStrWord = new Random().Next(1, 11650);
-            numStrWord = 11650;
+           // numStrWord = 11650;
             secretWord = File.ReadLines("WordsStockRus.txt").Skip(numStrWord - 1).First().Normalize().Trim();
             checkChar = new char[secretWord.Length];
             for (int i = 0; i < checkChar.Length; i++)
@@ -43,40 +43,50 @@ namespace Hangman_Game_HW
         {            
             for (;attempt > -1;)
             {
+                if (!checkChar.Contains<char>('*'))
+                {
+                    Console.Clear();
+                    Console.WriteLine("\nПоздравляю ты победил!\n");
+                    break;
+                }
+
                 if (attempt == 0)
                 {
+                    Console.Clear();
                     Console.WriteLine("Ты проиграл");
+                    break;
                 }
+                if (charContain == false && attempt != 6)
+                {
+                    Console.WriteLine("Такой буквы нет!");                    
+                }
+                Console.WriteLine($"Количество попыток: {attempt}\n");
                 Console.WriteLine("Загаданное слово:\n");
                 OutputWord();
                 Console.WriteLine("\nВведите букву");
                 tempStr = Console.ReadLine();
                 CheckSecretNumContainNum(tempStr.Normalize());
-               // Console.Clear();
+                Console.Clear();
             }
         }
 
         private void CheckSecretNumContainNum(string s)
         {
-            charContain = false;
-            if (secretWord.Contains(s))
-            {                
-                for (int i = 0; i < secretWord.Length; i++)
+            charContain = false;         
+            for (int i = 0; i < secretWord.Length; i++)
+            {
+                for (int j = 0; j < checkChar.Length; j++)
                 {
-                    for (int j = 0; j < checkChar.Length; j++)
+                    if (secretWord[i] == s[0])
                     {
-                        if (secretWord[i] == s[0])
-                        {
-                            checkChar[i] = s[0];
-                            charContain = true;
-                        }
+                        checkChar[i] = s[0];
+                        charContain = true;
                     }
-                    if (charContain == false)
-                    {
-                        Console.WriteLine("Такой буквы нет!");
-                        Console.WriteLine($"Количество попыток: {--attempt}");
-                    }                 
-                }
+                }                  
+            }
+            if (charContain == false)
+            {
+                --attempt;
             }
         } 
         private void OutputWord()
