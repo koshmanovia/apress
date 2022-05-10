06f6b91b8ad0595d7dd15836a36c9a7d90974485
Koshmanov Ilya <k0shmanov@yandex.ru>
2022-05-10 18:12:42 +0300

    lInq_with_parse_csv

---
+++ udemy/c#/PracticLINQwithParsing/PracticLINQwithParsing/ChessPlayer.cs
@@ -0,0 +1,32 @@
+﻿namespace PracticLINQwithParsing
+{
+    internal partial class Program
+    {
+        public class ChessPlayer
+        {
+            public string Country { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public int Id { get; set; }
+            public int BirthYear { get; set; }
+            public int Rating { get; set; }
+            public override string ToString()
+            {
+                return $"FullName: {FirstName} {LastName}, Rating = {Rating}, from {Country}, born in {BirthYear}";
+            }
+            public static ChessPlayer ParseFideCsv(string line)
+            {
+                string[] parts = line.Split(';');
+                return new ChessPlayer()
+                {
+                    Id = int.Parse(parts[0]),
+                    LastName = parts[1],
+                    FirstName = parts[2],
+                    Country = parts[4],
+                    Rating = int.Parse(parts[5]),
+                    BirthYear = int.Parse(parts[7])
+                };
+            }
+        }
+    }
+}
---
+++ udemy/c#/PracticLINQwithParsing/PracticLINQwithParsing/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticLINQwithParsing
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            MinMaxSumAverage(@"C:\MyProg\VS\projects\Apress\udemy\c#\PracticLINQwithParsing\PracticLINQwithParsing\top100ChessGamer.cvs");
+            Console.ReadLine();
+        }
+        static void MinMaxSumAverage(string file)
+        {
+            IEnumerable<ChessPlayer> list =  File.ReadAllLines(file)
+                                               .Skip(1)
+                                               .Select(ChessPlayer.ParseFideCsv)
+                                               .Where(player => player.BirthYear > 1988)
+                                               .OrderByDescending(player => player.Rating)
+                                               .Take(10)
+                                               .ToList();
+            Console.WriteLine($"The lowest rating in TOP 10 {list.Min(x=>x.Rating)}");
+            Console.WriteLine($"The lowest rating in TOP 10 {list.Max(x => x.Rating)}");
+            Console.WriteLine($"The lowest rating in TOP 10 {list.Average(x => x.Rating)}");
+        }
+    }
+}
