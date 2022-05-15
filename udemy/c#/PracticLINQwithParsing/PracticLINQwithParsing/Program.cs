1322bbd5efb4d70448034280459df2b8f11f8dce
Koshmanov Ilya <k0shmanov@yandex.ru>
2022-05-15 09:59:50 +0300

    Изучение ASP.net

--- udemy/c#/PracticLINQwithParsing/PracticLINQwithParsing/Program.cs
+++ udemy/c#/PracticLINQwithParsing/PracticLINQwithParsing/Program.cs
@@ -14,6 +14,19 @@ namespace PracticLINQwithParsing
             MinMaxSumAverage(@"C:\MyProg\VS\projects\Apress\udemy\c#\PracticLINQwithParsing\PracticLINQwithParsing\top100ChessGamer.cvs");
             Console.ReadLine();
         }
+        static void LinqDemo(string file)
+        {
+            IEnumerable<ChessPlayer> list = File.ReadAllLines(file)
+                                               .Skip(1)
+                                               .Select(ChessPlayer.ParseFideCsv)
+                                               .Where(player => player.BirthYear > 1988)
+                                               .OrderByDescending(player => player.Rating)
+                                               .Take(10)
+                                               .ToList();
+            Console.WriteLine($"The lowest rating in TOP 10 {list.Min(x => x.Rating)}");
+            Console.WriteLine($"The lowest rating in TOP 10 {list.Max(x => x.Rating)}");
+            Console.WriteLine($"The lowest rating in TOP 10 {list.Average(x => x.Rating)}");
+        }
         static void MinMaxSumAverage(string file)
         {
             IEnumerable<ChessPlayer> list =  File.ReadAllLines(file)
