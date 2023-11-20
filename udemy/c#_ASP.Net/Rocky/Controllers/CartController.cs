f477e0976d055a957f4bcc8ef754e00bd39f9d5e
Koshmanov Ilya <k0shmanov@yandex.ru>
2023-11-20 07:44:33 +0300

    View Index add snippets

--- udemy/c#_ASP.Net/Rocky/Controllers/CartController.cs
+++ udemy/c#_ASP.Net/Rocky/Controllers/CartController.cs
@@ -1,12 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+using Rocky.Utility;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Rocky.Controllers
 {
     public class CartController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CartController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            if(HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart) != null
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count() > 0)
+            {
+                shoppingCartList = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+            }
+            List<int> prodInCart = shoppingCartList.Select(i => i.ProductId).ToList();
+            IEnumerable<Product> prodList = _db.Product.Where(u => prodInCart.Contains(u.id));
+            return View(prodList);
         }
     }
 }
